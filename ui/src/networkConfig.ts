abad0bc28a4101c5fa1dad22fbc2e1048d414bcb
serdarates <serdar.ates@logo.com.tr>
2025-10-02 19:49:15 +0300

    package adress change

--- ui/src/networkConfig.ts
+++ ui/src/networkConfig.ts
@@ -14,7 +14,7 @@ import { createNetworkConfig } from "@mysten/dapp-kit";
  * │ Modules: arena, hero, marketplace
  * └──
  */
-const PACKAGE_ID = "0xa71b6e6886c2f72117432966906ffb34ce0fe3819e04d58ada637cb52f4bda2f";
+const PACKAGE_ID = "0xa92d939f53c60fdee24a2ecee67569ddbc05dbd4b012ffd2efb0d659d41d38e9";
 
 const { networkConfig, useNetworkVariable, useNetworkVariables } =
   createNetworkConfig({
